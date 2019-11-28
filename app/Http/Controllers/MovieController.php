e4e3d5b34344263589064ac5fff7199aaf56c471
Gísli Janus Jónsson <FS391958@fss.is>
2019-11-28 12:30:39 +0000

    Initial Commit

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MovieController extends Controller
+{
+    public function Show($slug)
+    {
+        $movie=DB::table('movies')->where('slug', $slug)->first();
+        dd($movie);
+        // if (! array_key_exists($movie, $movies)){
+        //     abort(404,"Sorry this page doesn't exsist.");
+        // }
+
+        return view('movie');
+
+    }
+    
+}
---
+++ database/migrations/2019_11_25_110859_create_movies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 255);
+            $table->float('rating');
+            $table->string('genre');
+            $table->text('description');
+            $table->string('poster', 255);
+            $table->date('release_date');
+            $table->string('slug', 255)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
